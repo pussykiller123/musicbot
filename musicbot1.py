918e71c5dc2c63845980c81f456348e9040cbc6a
pussykiller <alexey.lampa@gmail.com>
2018-05-07 05:25:30 +0700

    123 Change-Id: I5e842eac4f770325546660622e52f1bc99eb254d

--- musicbot1.py
+++ musicbot1.py
@@ -9,25 +9,25 @@ def get_updates_json(request):
 	return response.json()
 
 def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
+	results = data['result']
+	total_updates = len(results) - 1
+	return results[total_updates]
 
 def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
+	chat_id = update['message']['chat']['id']
+	return chat_id
 
 def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
+	params = {'chat_id': chat, 'text': text}
+	response = requests.post(url + 'sendMessage', data=params)
 
 def main():
-    update_id = last_update(get_updates_json(url))['update_id']
-    while True:
-        if update_id == last_update(get_updates_json(url))['update_id']:
-            send_mess(get_chat_id(last_update(get_updates_json(url))), 'А ща ещё лучше работать должно')
-            update_id += 1
-        sleep(1)
+	update_id = last_update(get_updates_json(url))['update_id']
+	while True:
+		if update_id == last_update(get_updates_json(url))['update_id']:
+			send_mess(get_chat_id(last_update(get_updates_json(url))), 'А ща ещё лучше работать должно')
+			update_id += 1
+		sleep(1)
 
 if __name__ == '__main__':
-    main()
+	main()
